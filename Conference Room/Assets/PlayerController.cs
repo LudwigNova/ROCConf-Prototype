26660dcbbd4a57d4a8b69ce218ddddf7d4e39efc
LudwigNova <lnova@u.rochester.edu>
2016-06-29 12:07:29 -0400

    ROConf Prototype before MasterServer work and OpenCV work

---
+++ Conference Room/Assets/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+	[SerializeField]
+	private float speed = 5f;
+	[SerializeField]
+	private float lookSensitivity = 3f;
+
+	private PlayerMotor motor;
+
+	void Start()
+	{
+		motor = GetComponent<PlayerMotor> ();
+	}
+
+	void Update()
+	{
+		//Calculate movement velocity as a 3D Vector
+		float _xMov = Input.GetAxisRaw("Vertical");
+		float _zMov = - Input.GetAxisRaw ("Horizontal");
+
+		Vector3 _movHorizontal = transform.right * _xMov;
+		Vector3 _movVertical = transform.forward * _zMov;
+
+		Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
+
+		motor.Move(_velocity);
+
+		float _yRot = Input.GetAxisRaw("Mouse X");
+
+		Vector3 _rotation = new Vector3 (0f, _yRot, 0f) * lookSensitivity;
+
+		motor.Rotate(_rotation);
+
+		float _xRot = Input.GetAxisRaw("Mouse Y");
+
+		Vector3 _cameraRotation = new Vector3 (_xRot, 0f, 0f) * lookSensitivity;
+
+		motor.RotateCamera(_cameraRotation);
+
+	}
+}
