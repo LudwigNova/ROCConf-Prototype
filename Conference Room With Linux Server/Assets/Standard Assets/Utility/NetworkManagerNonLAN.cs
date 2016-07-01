973a9a753922ed9c07cc2177c100d56dff6ec610
LudwigNova <lnova@u.rochester.edu>
2016-07-01 16:39:01 -0400

    Prototype with ability to connect to Linux Server
    Currently, the Unity application can connect to the linux server, but
    there are camera and spawning issues

---
+++ Conference Room With Linux Server/Assets/Standard Assets/Utility/NetworkManagerNonLAN.cs
@@ -0,0 +1,146 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class NetworkManagerNonLAN : NetworkBehaviour {
+
+	string registeredGameName = "ROC-HCI-Lab_Server";
+	bool isRefreshing = false;
+	float refreshRequestLength = 3.0f;
+	HostData[] hostData;
+
+//	Network.natFacilitatorIP = 13.82.52.184;
+//	Network.natFacilitatorPort = <facilitator port>;
+
+
+	//NetworkStartPosition[] spawnPoints;
+
+	private void StartServer()
+	{
+		//Connections: 16
+		//Listen Port: 25002
+		//useNat = false
+		////#########################################
+		////######USE THIS FOR THE LINUX SERVER######
+//		MasterServer.ipAddress = "13.82.52.184";
+//		MasterServer.port = 8080;
+
+		//Network.InitializeServer (16, 8080, false);
+		////#########################################
+		////#########################################
+
+		Network.InitializeServer (16, 25002, false);
+
+		MasterServer.RegisterHost (registeredGameName, "ROCConf Testing", "Test Server for ROCConf");
+		//spawnPoints = GameObject.FindObjectsOfType<NetworkStartPosition> ();
+	}
+
+	void OnServerInitialized()
+	{
+		Debug.Log ("Server has been initialized");
+		SpawnPlayer ();
+	}
+
+	void OnMasterServerEvent(MasterServerEvent masterServerEvent)
+	{
+		if (masterServerEvent == MasterServerEvent.RegistrationSucceeded) 
+		{
+			Debug.Log ("Registration Succesful!");
+		}
+	}
+
+	public IEnumerator RefreshHostList(){
+		Debug.Log ("Refreshing...");
+		MasterServer.RequestHostList (registeredGameName);
+		float timeStarted = Time.time;
+		float timeEnd = Time.time + refreshRequestLength;
+
+		while (Time.time < timeEnd) 
+		{
+			hostData = MasterServer.PollHostList ();
+			yield return new WaitForEndOfFrame ();
+		}
+
+		if (hostData == null || hostData.Length == 0) {
+			Debug.Log ("No active servers have been found.");
+		} else
+			Debug.Log (hostData.Length + " have been found.");
+	}
+
+	/////////////
+	/// Spawn and Player Server Communication
+	/// /////////
+
+	private void SpawnPlayer()
+	{
+	//	NetworkStartPosition playerSpawn = spawnPoints [Random.Range (0, spawnPoints.Length)];
+		Debug.Log ("Spawning Player...");
+		GameObject player = (GameObject)Network.Instantiate (Resources.Load ("Player"), new Vector3(0f, 2.5f, 0f), Quaternion.identity, 0);
+		//Camera.main.enabled = false;
+		//NetworkServer.SpawnWithClientAuthority (player, connectionToClient);
+	}
+
+	void OnClientConnect(){
+		SpawnPlayer ();
+	}
+
+	void OnPlayerDisconnected(NetworkPlayer player)
+	{
+		Debug.Log ("Player disconnected from: " + player.ipAddress + ";" + player.port);
+		Network.RemoveRPCs (player);
+		Network.DestroyPlayerObjects (player);
+	}
+
+	void OnApplicationQuit()
+	{
+		if (Network.isServer) 
+		{
+			Network.Disconnect (200);
+			MasterServer.UnregisterHost ();
+		}
+		if (Network.isClient) 
+		{
+			Network.Disconnect (200);
+		}
+	}
+
+	/// <summary>
+	/// GUI Settup
+	/// </summary>
+
+	public void OnGUI()
+	{
+		if (Network.isServer) {
+			GUILayout.Label ("Running as a server.");
+		} else if (Network.isClient) {
+			GUILayout.Label ("Running as a client");
+		}
+
+		if (Network.isClient) {
+//			if (GUI.Button (new Rect (25f, 25f, 150f, 30f), "Spawn"))
+//			{
+//				SpawnPlayer ();
+//			}
+		}
+
+		if (!Network.isClient && !Network.isServer) {
+
+			if (GUI.Button (new Rect (25f, 25f, 150f, 30f), "Start New Server")) {
+				StartServer ();
+
+			}
+
+			if (GUI.Button (new Rect (25f, 65f, 150f, 30f), "Refresh Server List")) {
+				StartCoroutine ("RefreshHostList");
+			}
+
+			if (hostData != null) {
+				for (int i = 0; i < hostData.Length; i++) {
+					if (GUI.Button (new Rect (Screen.width / 2, 65 + (30f * i), 300f, 30f), hostData [i].gameName)) {
+						Network.Connect (hostData [i]);
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Conference Room With Linux Server/Assets/WebcamTexture.cs
@@ -0,0 +1,11 @@
+﻿// Starts the default camera and assigns the texture to the current renderer
+using UnityEngine;
+using System.Collections;
+public class WebcamTexture : MonoBehaviour {
+	void Start() {
+		WebCamTexture webcamTexture = new WebCamTexture();
+		Renderer renderer = GetComponent<Renderer>();
+		renderer.material.mainTexture = webcamTexture;
+		webcamTexture.Play();
+	}
+}
